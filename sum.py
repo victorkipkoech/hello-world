cb302286aecdc8a942821d0dd798470145730de6
victorkipkoech <victor.kipkoech94@gmail.com>
2017-06-11 20:14:07 -0700

    learning in progress

---
+++ sum.py
@@ -0,0 +1,8 @@
+postalCode =''
+message ='hello  world'
+print(message.find('world'))
+print(message.count('l'))
+print(message.capitalize())
+print(message.replace('hello','Hi!'))
+postalCode=input('Enter your postal code:\n')
+print (postalCode.upper())
